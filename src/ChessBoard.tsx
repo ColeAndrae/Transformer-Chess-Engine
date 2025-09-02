9237f5090c18e775f9748be3defc6eaa06b17cb4
Cole Andrae <cole@theandraes.com>
2025-09-02 12:22:14 -0700

    added chess pieces

--- src/ChessBoard.tsx
+++ src/ChessBoard.tsx
@@ -1,4 +1,45 @@
+import { useEffect, useState } from "react";
+
 function ChessBoard() {
+  interface StringDictionary {
+    [key: string]: string;
+  }
+  const blackKing = "/Transformer-Chess-Engine/king-b.svg";
+  const whiteKing = "/Transformer-Chess-Engine/king-w.svg";
+  const blackQueen = "/Transformer-Chess-Engine/queen-b.svg";
+  const whiteQueen = "/Transformer-Chess-Engine/queen-w.svg";
+  const blackKnight = "/Transformer-Chess-Engine/knight-b.svg";
+  const whiteKnight = "/Transformer-Chess-Engine/knight-w.svg";
+  const blackRook = "/Transformer-Chess-Engine/rook-b.svg";
+  const whiteRook = "/Transformer-Chess-Engine/rook-w.svg";
+  const blackBishop = "/Transformer-Chess-Engine/bishop-b.svg";
+  const whiteBishop = "/Transformer-Chess-Engine/bishop-w.svg";
+  const blackPawn = "/Transformer-Chess-Engine/pawn-b.svg";
+  const whitePawn = "/Transformer-Chess-Engine/pawn-w.svg";
+  const [gameState, setGameState] = useState([
+    ["r", "n", "b", "q", "k", "b", "n", "r"],
+    ["p", "p", "p", "p", "p", "p", "p", "p"],
+    ["-", "-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-", "-"],
+    ["P", "P", "P", "P", "P", "P", "P", "P"],
+    ["R", "N", "B", "Q", "K", "B", "N", "R"],
+  ]);
+  const pieces: StringDictionary = {
+    k: blackKing,
+    K: whiteKing,
+    q: blackQueen,
+    Q: whiteQueen,
+    n: blackKnight,
+    N: whiteKnight,
+    r: blackRook,
+    R: whiteRook,
+    b: blackBishop,
+    B: whiteBishop,
+    p: blackPawn,
+    P: whitePawn,
+  };
   function GenerateBoard() {
     let TempBoard: any[] = [];
     for (var r = 0; r < 8; r++) {
@@ -7,8 +48,11 @@ function ChessBoard() {
         let TileColor = (r + c) % 2 == 0 ? "#eeeed2" : "#769656";
         TempRow[c] = (
           <div
-            style={{ backgroundColor: TileColor }}
-            className="w-20 h-20"
+            className="w-20 h-20 bg-center bg-no-repeat bg-cover"
+            style={{
+              backgroundImage: `url('${pieces[gameState[r][c]]}')`,
+              backgroundColor: TileColor,
+            }}
           ></div>
         );
       }
@@ -17,7 +61,11 @@ function ChessBoard() {
     return <div className="flex-col">{TempBoard}</div>;
   }
   return (
-    <div className="mx-auto my-16 justify-items-center">{GenerateBoard()}</div>
+    <div>
+      <div className="mx-auto my-16 justify-items-center">
+        {GenerateBoard()}
+      </div>
+    </div>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Transformer-Chess-Engine'
+  base: '/Transformer-Chess-Engine/'
 })
